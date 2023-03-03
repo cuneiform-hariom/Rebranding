73e97b544736a10c6e87b4bcbf0b215e49e4524b
hariom <hariomcuneiform@gmail.com>
2023-03-03 16:47:56 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,124 @@
+// jquery for client logo slider 
+
+$(document).ready(function () {
+    $('.customer-logos').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2,
+            }
+        }]
+    });
+});
+
+// testimonial slider 
+
+
+$(document).ready(function () {
+    $('.testi-slider-wrap').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
+// hero slider 
+$(document).ready(function () {
+    $('.slider-wrap').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
+
+// tech read slider 
+
+$(document).ready(function () {
+    $('.all-tech').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
+
+// ==============================
+
+$(document).on('click', '.tab-list li a', function() {
+    var $this = $(this),
+        $tabList = $this.parents('ul'),
+        _idx = $this.closest('li').index();
+    
+    $tabList.children().eq(_idx).addClass('in').siblings().removeClass('in');
+    $tabList.next().children().eq(_idx).addClass('in').siblings().removeClass('in');
+  });
