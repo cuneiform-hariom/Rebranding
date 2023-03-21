a5fac5c47bae75899cbcacaca70a4f0b8d1d9f99
hariom <hariomcuneiform@gmail.com>
2023-03-21 17:47:56 +0530

    about page and many more

--- js/script.js
+++ js/script.js
@@ -112,6 +112,31 @@ $(document).ready(function () {
     });
 });
 
+// solution slider 
+
+$(document).ready(function () {
+    $('.pp-sol-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
+
 // ==============================
 
 $(document).on('click', '.tab-list li a', function() {
